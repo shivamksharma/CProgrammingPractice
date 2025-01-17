1e04a6201b08c84093452afd34883ffdeaa511d2
Krish Kumar <krishkumar2306@gmail.com>
2025-01-17 20:54:13 +0530

    feat: adding new program

---
+++ Q34.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+	int n;
+	printf("Enter the number\n");
+	for(int i=1; i<=10; i++) {
+		printf("%dx%d=%d\n", n,n*i,i);
+	}
+	/* code */
+	return 0;
+}
