978c5aeaeb446580527380f7553e75ffaad7a737
Anonymous <53265299+shivamksharma@users.noreply.github.com>
2025-01-17 21:10:19 +0530

    feat: adding new program

---
+++ CP_002.c
@@ -0,0 +1,37 @@
+// Factorial of a given number
+
+#include <stdio.h>
+
+int main() {
+    int num;
+    int factorial = 1;
+
+    printf("Enter a positive integer: ");
+    scanf("%d", &num);
+
+    if (num < 0) {
+        printf("Factorial is not defined for negative numbers.\n");
+
+    } else {
+        printf("%d! = ", num);
+
+        for (int i = num; i >= 1; i--) {
+            printf("%d", i);
+
+            if (i > 1) {
+                printf("x");
+
+            }
+
+            factorial *= i;
+
+        }
+
+        printf(" = %d\n", factorial);
+
+    }
+
+    return 0;
+
+}
+
