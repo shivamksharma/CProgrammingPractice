8e47de8ff5be01fc46c3b6b474558609910fbfe9
Anonymous <53265299+shivamksharma@users.noreply.github.com>
2025-01-18 09:15:30 +0530

    feat: adding CP_025

---
+++ CP_026.c
@@ -0,0 +1,22 @@
+/* Program to read and display n number using an array */
+
+#include <stdio.h>
+int main()
+{
+    int i=0, n, arr[20];
+    printf("\n Enter the number of elements:");
+    scanf("%d", &n);
+
+    for(i=0;i<n;i++)
+    {
+        printf("\n Arr[%d] = ", i);
+        scanf("%d", &arr[i]);
+    }
+        printf("\n The array elements are ");
+        
+        for(i=0;i<n;i++)
+        printf("Arr[%d] = %d\t", i, arr[i]);
+        return 0;
+}
+
+
