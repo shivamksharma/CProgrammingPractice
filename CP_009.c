d01a301b41fed73115cd78db7577d4324d798007
Anonymous <53265299+shivamksharma@users.noreply.github.com>
2025-01-18 10:26:23 +0530

    feat: modifying and adding comment Co-authored-by: shivamksharma <shivam.sk23.09@gmail.com> Co-authored-by: krishkumar23 <krishkumar2306@gmail.com>

--- CP_009.c
+++ CP_009.c
@@ -1,25 +1,20 @@
-// Math Tools in C HCF (or GCD)
-
+// WAP to find HCF of two numbers
 
 #include <stdio.h>
 
-int hcf(int a, int b)
-{
-    return (b == 0) ? a : hcf(b, a % b);
-}
+int hcf(int a, int b) { return (b == 0) ? a : hcf(b, a % b); }
 
-int main()
-{
-    int a, b;
+int main() {
+  int a, b;
 
-    printf("Enter the first number  : ");
-    scanf("%i", &a);
+  printf("Enter the first number  : ");
+  scanf("%i", &a);
 
-    printf("Enter the second number : ");
-    scanf("%i", &b);
-    
-    printf("HCF of %i and %i is %i", a, b, hcf(a, b));
+  printf("Enter the second number : ");
+  scanf("%i", &b);
 
-    getch();
-    return 0;
+  printf("HCF of %i and %i is %i", a, b, hcf(a, b));
+
+  getch();
+  return 0;
 }
