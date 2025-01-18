510d6cda78fda847569c5a693da1ebb9b1a0a1c7
Anonymous <53265299+shivamksharma@users.noreply.github.com>
2025-01-18 08:57:58 +0530

    feat: adding CP_011

---
+++ CP_011.c
@@ -0,0 +1,39 @@
+/*
+ *  Math Tools in C 
+ *      LCM
+ *
+ *  https://afaan.dev/math-tools-in-c 
+ * 
+ *  (c) Afaan Bilal (https://afaan.dev)
+ *
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    int a, b, LCM;
+
+    printf("Enter the first number  : ");
+    scanf("%i", &a);
+
+    printf("Enter the second number : ");
+    scanf("%i", &b);
+
+    LCM = (a > b) ? a : b;
+
+    while (1)
+    {
+        if (LCM % a == 0 && LCM % b == 0)
+        {
+            printf("The LCM of %i and %i is %i", a, b, LCM);
+            
+            break;
+        }
+        else
+            LCM++; 
+    }
+
+    getch();
+    return 0;
+}
