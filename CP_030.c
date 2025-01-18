d4babca2dcad8c8c52202888b708933f852cafef
Anonymous <53265299+shivamksharma@users.noreply.github.com>
2025-01-18 12:47:31 +0530

    feat: adding CP_030

---
+++ CP_030.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+#define MAX_SIZE 50 
+
+int main()
+{
+    int arr[MAX_SIZE];           
+    int size;                   
+    int i, j, k;                
+
+   
+    printf("Enter size of the array : ");
+    scanf("%d", &size);
+
+    
+    printf("Enter elements in array : ");
+    for(i=0; i<size; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+
+    // Find duplicate elements in array
+    for(i=0; i<size; i++)
+    {
+        for(j=i+1; j<size; j++)
+        {
+           
+            if(arr[i] == arr[j])
+            {
+                
+                for(k=j; k < size - 1; k++)
+                {
+                    arr[k] = arr[k + 1];
+                }
+               
+                size--;
+
+                j--;
+            }
+        }
+    }
+
+
+    // Print array after deleting duplicates
+    printf("\nArray elements after deleting duplicates : ");
+    for(i=0; i<size; i++)
+    {
+        printf("%d\t", arr[i]);
+    }
+
+    return 0;
+}
