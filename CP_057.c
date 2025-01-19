f257592f6184e915164c49038082cc6f6e014beb
Anonymous <53265299+shivamksharma@users.noreply.github.com>
2025-01-19 10:22:02 +0530

    feat: adding CP_057

---
+++ CP_057.c
@@ -0,0 +1,24 @@
+/* Program find the factorial number using Function */
+
+#include<stdio.h>
+
+int factorial(int);
+
+int main()
+{
+    int num, fact;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    fact=factorial(num);
+    printf("Factorial of %d is: %d\n", num, fact);
+    return 0;
+}
+int factorial(int n)
+{
+    int i, fact=1;
+    for(i=1; i<=n; i++)
+    {
+        fact=fact*i;
+    }
+    return fact;
+}
