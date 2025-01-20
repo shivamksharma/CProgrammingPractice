4bc9e0b38066fba7ebc9dcf8cd1d3b6378e90695
Anonymous <53265299+shivamksharma@users.noreply.github.com>
2025-01-20 08:01:15 +0530

    feat: adding CP_075

---
+++ CP_075.c
@@ -0,0 +1,9 @@
+/* Write a program to read an integer. Then display the value of that integer in decimal, octal, and hexadecimal notations  */
+
+#include<stdio.h>
+int main()
+{
+	int num = 10;
+	printf("\n Decimal Value is %d \t Octal Value is %o \t Hexadecimal Value is %x", num, num, num);
+}
+
