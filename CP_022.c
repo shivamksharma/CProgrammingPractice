a743b124eb2274cebe989e161f9f2c7661ea365a
Anonymous <53265299+shivamksharma@users.noreply.github.com>
2025-01-21 09:52:56 +0530

    chores: fixing and updating

--- CP_022.c
+++ CP_022.c
@@ -1,74 +1,71 @@
-/* Write a program to convert the roman numbers into normal numbers */
+// WAP to convert roman numbers to decimal numbers
 
 #include <stdio.h>
 #include <string.h>
 
-int convert(char); 
+int convert(char);
 
-int main ()
-{
-   int i = 0, num = 0;
-   char romanNumber[100];
+int main() {
+  int i = 0, num = 0;
+  char romanNumber[100];
 
-   printf("Enter Roman number: ");
-   scanf("%s", romanNumber);
+  printf("Enter Roman number: ");
+  scanf("%s", romanNumber);
 
-   while(romanNumber[ i ])
-   {
-      
-      if(convert(romanNumber[ i ]) < 0 )
-      {
-         printf("\nInvalid Roman Number.\n");
-         return 0;
+  while (romanNumber[i]) {
+
+    if (convert(romanNumber[i]) < 0) {
+      printf("\nInvalid Roman Number.\n");
+      return 0;
+    }
+
+    if ((strlen(romanNumber) - i) > 2) {
+      if (convert(romanNumber[i]) < convert(romanNumber[i + 2])) {
+        printf("\nInvalid Roman Number.\n");
+        return 0;
       }
+    }
 
-      
-      if((strlen(romanNumber) - i ) > 2)
-      {
-         if(convert(romanNumber[ i ]) < convert(romanNumber[i + 2]))
-         {
-            printf("\nInvalid Roman Number.\n");
-            return 0;
-         }
-      }
-
-      
-      if(convert(romanNumber[ i ]) >= convert(romanNumber[i + 1])) 
-         num = num + convert(romanNumber[ i ]);
-      else
-      {
-         num = num + (convert(romanNumber[i + 1]) - convert(romanNumber[ i ]));
-         i++;
-      }
+    if (convert(romanNumber[i]) >= convert(romanNumber[i + 1]))
+      num = num + convert(romanNumber[i]);
+    else {
+      num = num + (convert(romanNumber[i + 1]) - convert(romanNumber[i]));
       i++;
-   }
+    }
+    i++;
+  }
 
-   
-   printf("\nThe Decimal Number: %d\n", num);
-   return 0;
+  printf("\nThe Decimal Number: %d\n", num);
+  return 0;
 }
 
-int convert(char ch)
-{
-   int value = 0;
+int convert(char ch) {
+  int value = 0;
 
-   switch(ch)
-   {
-      case 'I': value = 1; 
-      break;
-      case 'V': value = 5; 
-      break;
-      case 'X': value = 10; 
-      break;
-      case 'L': value = 50; 
-      break;
-      case 'C': value = 100; 
-      break;
-      case 'D': value = 500; 
-      break;
-      case 'M': value = 1000; 
-      break;
-      default: value = -1;
-   }
-   return value;
+  switch (ch) {
+  case 'I':
+    value = 1;
+    break;
+  case 'V':
+    value = 5;
+    break;
+  case 'X':
+    value = 10;
+    break;
+  case 'L':
+    value = 50;
+    break;
+  case 'C':
+    value = 100;
+    break;
+  case 'D':
+    value = 500;
+    break;
+  case 'M':
+    value = 1000;
+    break;
+  default:
+    value = -1;
+  }
+  return value;
 }
