7ead3d31c77f32caf6397a5a72de400c60060c90
Anonymous <53265299+shivamksharma@users.noreply.github.com>
2025-01-21 09:47:39 +0530

    chores: fixing and updating

--- CP_018.c
+++ CP_018.c
@@ -1,19 +1,18 @@
-/* Program for negative and positive number using function */
+// WAP to check whether a number is positive, negative or zero
 
-#include<stdio.h>
+#include <stdio.h>
 
-int main()
-{
-    int num;
-    printf("Enter a number: ");
-    scanf("%d",&num);
-    
-    if(num>0)
-        printf("%d is a positive number",num);
-    else if(num<0)
-        printf("%d is a negative number",num);
-    else
-        printf("%d is zero",num);
-    
-    return 0;
+int main() {
+  int num;
+  printf("Enter a number: ");
+  scanf("%d", &num);
+
+  if (num > 0)
+    printf("%d is a positive number", num);
+  else if (num < 0)
+    printf("%d is a negative number", num);
+  else
+    printf("%d is zero", num);
+
+  return 0;
 }
