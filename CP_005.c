07225c64e7ae200e3efb2e211662fc784715c4a4
Anonymous <53265299+shivamksharma@users.noreply.github.com>
2025-01-21 09:35:45 +0530

    chores: fixing and updating

--- CP_005.c
+++ CP_005.c
@@ -1,22 +1,20 @@
-// GCD program using recursion
-
+// WAP to print GCD of two numbers using recursion
 
 #include <stdio.h>
-int gcd(int a,int b)
-{
-	if(b==0)
-	return a;
-	else return gcd(b,a%b);
-	return gcd(b,a%b);
+int gcd(int a, int b) {
+  if (b == 0)
+    return a;
+  else
+    return gcd(b, a % b);
+  return gcd(b, a % b);
 }
-int main()
-{
-	int i,temp,a,b,min;
-	printf("Enter the two no.s a and b:");
-	scanf("%d%d",&a,&b);
-	min=a;
-	if(a>b)
-	min=b;
-	temp=gcd(a,b);
-	printf("Gcd of two numbers is :%d\n",temp);
+int main() {
+  int i, temp, a, b, min;
+  printf("Enter the two no.s a and b:");
+  scanf("%d%d", &a, &b);
+  min = a;
+  if (a > b)
+    min = b;
+  temp = gcd(a, b);
+  printf("Gcd of two numbers is :%d\n", temp);
 }
