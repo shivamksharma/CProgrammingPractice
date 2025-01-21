d11fe9952950dc33dd6ba16d801044d911c47524
Anonymous <53265299+shivamksharma@users.noreply.github.com>
2025-01-21 09:33:53 +0530

    chores: fixing and updating

--- CP_004.c
+++ CP_004.c
@@ -1,21 +1,17 @@
-// Greatest Common Divisor of two or more numbers 
-
+// WAP to find the GCD of two or more numbers
 
 #include <stdio.h>
-int main()
-{
-	int i,temp,a,b,min;
-	printf("Enter the two no.s a and b:");
-	scanf("%d%d",&a,&b);
-	min=a;
-	if(a>b)
-	min=b;
-	for(i=1;i<=min;i++)
-	{
-		if(a%i==0&&b%i==0)
-		{
-			temp=i;
-		}
-	}
-	printf("GCD of two no.s is :%d\n",temp);
+int main() {
+  int i, temp, a, b, min;
+  printf("Enter the two no.s a and b:");
+  scanf("%d%d", &a, &b);
+  min = a;
+  if (a > b)
+    min = b;
+  for (i = 1; i <= min; i++) {
+    if (a % i == 0 && b % i == 0) {
+      temp = i;
+    }
+  }
+  printf("GCD of two no.s is :%d\n", temp);
 }
