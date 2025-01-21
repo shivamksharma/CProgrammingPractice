b31c670d806573bac3982e5a86d1023666be8cd9
Anonymous <53265299+shivamksharma@users.noreply.github.com>
2025-01-21 09:49:57 +0530

    chores: fixing and updating

--- CP_019.c
+++ CP_019.c
@@ -1,25 +1,23 @@
-/* Find the odd and even number and pass by using function */
+// WAP to check if a number is even or odd using function
 
-#include<stdio.h>
+#include <stdio.h>
 
-int isEven(int num)
-{
-    if(num%2==0)
-        return 1;
-    else
-        return 0;
+int isEven(int num) {
+  if (num % 2 == 0)
+    return 1;
+  else
+    return 0;
 }
 
-int main()
-{
-    int num;
-    printf("Enter a number: ");
-    scanf("%d",&num);
-    
-    if(isEven(num))
-        printf("%d is an even number",num);
-    else
-        printf("%d is an odd number",num);
-    
-    return 0;
+int main() {
+  int num;
+  printf("Enter a number: ");
+  scanf("%d", &num);
+
+  if (isEven(num))
+    printf("%d is an even number", num);
+  else
+    printf("%d is an odd number", num);
+
+  return 0;
 }
