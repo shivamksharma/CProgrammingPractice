a9f2cd45807616e549b837c01c3331fd0d73b3bc
Anonymous <53265299+shivamksharma@users.noreply.github.com>
2025-01-21 09:54:38 +0530

    chores: fixing and updating

--- CP_024.c
+++ CP_024.c
@@ -1,38 +1,26 @@
-/* write a menu driven program to add, subtract, multiply, divide two integer using function */ 
-#include<stdio.h>
-int add(int,int);
-int sub(int,int);
-int mul(int,int);
-int div(int,int);
+// WAP to add, subtract, multiply, divide two integer using function
 
-int main()
-{
-    int a,b,c,d,e,f;
-    printf("Enter two numbers\n");
-    scanf("%d%d",&a,&b);
-    c=add(a,b);
-    d=sub(a,b);
-    e=mul(a,b);
-    f=div(a,b);
-    printf("Addition of two numbers is %d\n",c);
-    printf("Subtraction of two numbers is %d\n",d);
-    printf("Multiplication of two numbers is %d\n",e);
-    printf("Division of two numbers is %d\n",f);
-    return 0;
+#include <stdio.h>
+int add(int, int);
+int sub(int, int);
+int mul(int, int);
+int div(int, int);
+
+int main() {
+  int a, b, c, d, e, f;
+  printf("Enter two numbers\n");
+  scanf("%d%d", &a, &b);
+  c = add(a, b);
+  d = sub(a, b);
+  e = mul(a, b);
+  f = div(a, b);
+  printf("Addition of two numbers is %d\n", c);
+  printf("Subtraction of two numbers is %d\n", d);
+  printf("Multiplication of two numbers is %d\n", e);
+  printf("Division of two numbers is %d\n", f);
+  return 0;
 }
-int add(int a,int b)
-{
-    return a+b;
-}
-int sub(int a,int b)
-{
-    return a-b;
-}
-int mul(int a,int b)
-{
-    return a*b;
-}
-int div(int a,int b)
-{
-    return a/b;
-}
+int add(int a, int b) { return a + b; }
+int sub(int a, int b) { return a - b; }
+int mul(int a, int b) { return a * b; }
+int div(int a, int b) { return a / b; }
