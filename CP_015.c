91ba0fe9a7d2c5a06732159fbf09aa23dd7910ef
Anonymous <53265299+shivamksharma@users.noreply.github.com>
2025-01-21 09:46:15 +0530

    chores: fixing and updating

--- CP_015.c
+++ CP_015.c
@@ -1,34 +1,22 @@
-/*
- *  Math Tools in C 
- *      nCr
- *
- *  https://afaan.dev/math-tools-in-c 
- * 
- *  (c) Afaan Bilal (https://afaan.dev)
- *
- */
+// WAP to nCr (n choose r) assert n >= r >= 0
 
 #include <stdio.h>
 
-unsigned long int factorial(unsigned int n)
-{
-	return (n == 0) ? 1 : n * factorial(n - 1);
+unsigned long int factorial(unsigned int n) {
+  return (n == 0) ? 1 : n * factorial(n - 1);
 }
 
-unsigned long int ncr(unsigned int n, unsigned int r)
-{
-    return factorial(n) / ( factorial(n - r) * factorial(r) );
+unsigned long int ncr(unsigned int n, unsigned int r) {
+  return factorial(n) / (factorial(n - r) * factorial(r));
 }
 
-int main()
-{
-    unsigned int n, r;
-    
-    printf("Enter n and r (space separated): ");
-    scanf("%d %d", &n, &r);
-    
-    printf("nCr for n = %d and r = %d is %d", n, r, ncr(n, r));
-    
-    getch();
-    return 0;
+int main() {
+  unsigned int n, r;
+
+  printf("Enter n and r (space separated): ");
+  scanf("%d %d", &n, &r);
+
+  printf("nCr for n = %d and r = %d is %d", n, r, ncr(n, r));
+
+  return 0;
 }
